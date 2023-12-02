38d67492a61ca99226fa6b61a970206f92ea8fd3
TheDrowsyDev <vyperius117@gmail.com>
2023-12-02 09:51:12 -0500

    Added code and documentation

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # PythonCLICalculator
 Simple CLI Calculator in Python
+
+This application serves as a basic introduction to building a CLI application with `argparse` in Python. All the code is located in `src/main.py`.
+
+Please feel free to submit any question or comments as issues. Happy coding!
+
+## Usage
+```
+usage: main.py [-h] {add,subtract,multiply,divide} num_one num_two
+
+positional arguments:
+  {add,subtract,multiply,divide}
+                        The operation to run on the given arguments.
+  num_one               The first number.
+  num_two               The second number.
+
+options:
+  -h, --help            show this help message and exit
+```
+
+Add:
+```
+python3 main.py add 1.0 5.0
+Result: 6.0
+```
+
+Subtract:
+```
+python3 main.py subtract 5.0 3.0
+Result: 2.0
+```
+
+Multiply:
+```
+python3 main.py multiply 5 5
+Result: 25.0
+```
+
+Divide:
+```
+python3 main.py divide 9.0 3.0
+Result: 3.0
+```
---
+++ src/main.py
@@ -0,0 +1,103 @@
+'''
+This script will allow users to run a simple
+calculator app with supplied arguments on the 
+command line.
+'''
+
+__author__ = "Drowsy Dev"
+__version__ = 1.0
+
+from argparse import ArgumentParser
+
+def add(num1: float, num2: float) -> float:
+    '''Adds two floats together and return the sum.
+
+       :param num1: float, The first number to add
+       :param num2: float, The second number to add
+
+       :return sum: float, The sum of the supplied arguments
+
+       Example Usage:
+       ```
+        sum = add(1.0, 2.0)
+        print(sum) # 3.0
+       ```
+    '''
+    sum = num1 + num2
+    return sum
+
+def subtract(num1: float, num2: float) -> float:
+    '''Subtract two floats and return the difference.
+
+       :param num1: float, The number to subtract from
+       :param num2: float, The number to subtract
+
+       :return remainder: float, The difference of the supplied arguments
+
+       Example Usage:
+       ```
+        difference = subtract(5.0, 3.0)
+        print(difference) # 2.0
+       ```
+    '''
+    difference = num1 - num2
+    return difference
+
+def multiply(num1: float, num2: float) -> float:
+    '''Multiplies two floats together and return the product.
+
+       :param num1: float, The first number to multiply
+       :param num2: float, The second number to multiply
+
+       :return product: float, The product of the supplied arguments
+
+       Example Usage:
+       ```
+        product = multiply(3.0, 2.0)
+        print(product) # 6.0
+       ```
+    '''
+    product = num1 * num2
+    return product
+
+def divide(num1: float, num2: float) -> (float | str):
+    '''Divides two floats and return the quotient.
+
+       :param num1: float, The number to divide from
+       :param num2: float, The number to divide by
+
+       :return quotient: float, The quotient of the supplied arguments
+
+       Example Usage:
+       ```
+        quotient = divide(6.0, 2.0)
+        print(quotient) # 3.0
+       ```
+    '''
+    if num2 == 0:
+        print("Error: Divide by zero error")
+        return "inf"
+    quotient = num1 / num2
+    return quotient
+
+parser = ArgumentParser()
+parser.add_argument("operation", type=str, choices=["add", "subtract", "multiply", "divide"], help="The operation to run on the given arguments.")
+parser.add_argument("num_one", type=float, help="The first number.")
+parser.add_argument("num_two", type=float, help="The second number.")
+
+args = parser.parse_args()
+operation = args.operation
+num_one = args.num_one
+num_two = args.num_two
+
+result = None
+if operation == "add":
+    result = add(num_one, num_two)
+elif operation == "subtract":
+    result = subtract(num_one, num_two)
+elif operation == "multiply":
+    result = multiply(num_one, num_two)
+else:
+    result = divide(num_one, num_two)
+
+print(f"Result: {result}")
